03e20735cec82dd641c2f7303728927057cdc8b3
Dreeam <61569423+Dreeam-qwq@users.noreply.github.com>
2025-07-27 13:41:06 +0800

    Remove dev bundle
    Needs to find another way to be able to use internal code

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     `java-library`
     id("com.gradleup.shadow") version "9.0.0-rc1"
-    //id("io.papermc.paperweight.userdev") version "2.0.0-beta.18"
 }
 
 group = "cn.dreeam.sunbox"
@@ -9,20 +8,15 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     //mavenCentral() // Maven Central
-    maven("https://maven-central.storage-download.googleapis.com/maven2") // Google Mirror - For US
-    //maven("https://maven.aliyun.com/repository/public") // Aliyun Mirror - For CN
-
-    maven("https://maven.leafmc.one/snapshots/")
+    //maven("https://maven-central.storage-download.googleapis.com/maven2") // Google Mirror - For US
+    maven("https://maven.aliyun.com/repository/public") // Aliyun Mirror - For CN
 }
 
 dependencies {
     implementation("org.openjdk.jmh:jmh-core:1.37")
     annotationProcessor("org.openjdk.jmh:jmh-generator-annprocess:1.37")
 
-    // TODO: Notice: Enable if you needs to access Leaf internal
-    //paperweight.devBundle("cn.dreeam.leaf", "1.21.8-R0.1-SNAPSHOT")
-
-    //implementation("com.github.oshi:oshi-core:6.8.2")
+    api("it.unimi.dsi:fastutil:8.5.15")
     api("com.google.guava:guava:33.4.0-jre")
     api("org.jetbrains:annotations:26.0.2")
 }
