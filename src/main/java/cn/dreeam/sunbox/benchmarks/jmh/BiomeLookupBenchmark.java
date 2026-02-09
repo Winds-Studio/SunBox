cf6d1818e69e4e622d66258fca8606ffd786e380
HaHaWTH <102713261+HaHaWTH@users.noreply.github.com>
2026-02-09 16:44:04 +0800

    Reduce noise

--- src/main/java/cn/dreeam/sunbox/benchmarks/jmh/BiomeLookupBenchmark.java
+++ src/main/java/cn/dreeam/sunbox/benchmarks/jmh/BiomeLookupBenchmark.java
@@ -2,7 +2,6 @@ package cn.dreeam.sunbox.benchmarks.jmh;
 
 import org.openjdk.jmh.annotations.*;
 import org.openjdk.jmh.infra.Blackhole;
-
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
@@ -14,12 +13,12 @@ public class BiomeLookupBenchmark {
     private static final double[] QUART_OFFSETS = {0.0D, 0.25D, 0.5D, 0.75D};
 
     private int[] inputData;
-    private static final int SIZE = 4096; // Size of input array
-    private static final Random rand = new Random(12345L);
+    private static final int SIZE = 4096;
 
     @Setup
     public void setup() {
         inputData = new int[SIZE];
+        Random rand = new Random(12345L);
         for (int i = 0; i < SIZE; i++) {
             inputData[i] = rand.nextInt();
         }
@@ -27,17 +26,19 @@ public class BiomeLookupBenchmark {
 
     @Benchmark
     public void testFpDivision(Blackhole bh) {
+        double sum = 0;
         for (int i = 0; i < SIZE; i++) {
-            int val = inputData[i];
-            bh.consume((double) (val & 3) / 4.0);
+            sum += (double) (inputData[i] & 3) / 4.0;
         }
+        bh.consume(sum);
     }
 
     @Benchmark
     public void testTableLookup(Blackhole bh) {
+        double sum = 0;
         for (int i = 0; i < SIZE; i++) {
-            int val = inputData[i];
-            bh.consume(QUART_OFFSETS[val & 3]);
+            sum += QUART_OFFSETS[inputData[i] & 3];
         }
+        bh.consume(sum);
     }
 }
